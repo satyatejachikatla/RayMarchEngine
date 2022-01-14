3640dff658018ac4b0df89ea02b080b8f8eb3469
satyatejachikatla <satyatejachikatla@gmail.com>
2022-01-14 16:36:36 +0530

    Added basic 3d ray march shape functions

--- main.glsl
+++ main.glsl
@@ -1,25 +1,51 @@
 #include <rayMarchUtils/core.hglsl>
 #include <rayMarchUtils/utils.hglsl>
-#include <rayMarchUtils/math/math.hglsl>
-#include <rayMarchUtils/math/spaceTransforms.hglsl>
+#include <rayMarchUtils/shapes/shapes3D.hglsl>
+
+float GetDist(vec3 p){
+
+	float sphereDist1 = SDSphere(p,vec3(0.,1.,6.),1.);
+	float sphereDist2 = SDSphere(p,vec3(2.1,1.,6.),1.);
+	float planeDist = SDXZPlaneAtYZero(p);
+	float cd = SDCapsule(p,vec3(0.,1.,6.),vec3(1.,2.,2.),.2);
+	float td = SDTorus(p-vec3(.0,.5,6.),vec2(1.5,.25));
+	float bd = SDBox(p-vec3(-2.5,.5,6.),vec3(.75));
+	float cyld = SDCylinder(p-vec3(.5,.5,.5),vec3(0.,.3,.0),vec3(1.,2.,2.),.1);
+
+	float d = sphereDist1;
+	d = min(d,sphereDist2);
+	d = min(d,planeDist);
+	d = min(d,cd);
+	d = min(d,td);
+	d = min(d,bd);
+	d = min(d,cyld);
+	
+	return d;
+
+}
 
 void main()
 {
 	vec2 uv = getPixelNormalized();
 	vec2 mouse = getMouseNormalized();
-	float sec = getTime();
+	float time = getTime();
 
-	Camera3D cam;
-   	cam.zoom = 1.; // No zoom
-    cam.lookAt = vec3(-mouse,1.);
-    cam.rayOrigin = vec3(-0.,1.,-0);
-    cam.upWorld = vec3(0.,1.,0.); // Up of the worl will always be y
+	Camera3D cam = getDefaultCamera3D();
+	RayMarchSettings rayMarchSettings = getDefaultRayMarchSettings();
 
-	vec3 rayDirection = getCamera3DRay(cam); // returns the ray direction for the current pixel
+	// Camera with mouse
+	cam.lookAt = vec3(-mouse.x,mouse.y,0.0);
+	cam.rayOrigin = vec3(-1.,1.,-1.);
 
-	float rayHitDistance = RayMarch(cam.rayOrigin,rayDirection);
+	// Light Source Movement
+	time = time * 2.;
+	rayMarchSettings.lightSource = vec3(3.*sin(time),5.,2.*cos(time));
+
+	vec3 rayDirection = getCamera3DRay(cam);
+
+	float rayHitDistance = RayMarch(cam.rayOrigin,rayDirection,rayMarchSettings);
 	vec3 rayHitPosition = cam.rayOrigin + rayDirection * rayHitDistance;
-	float l = GetLight(rayHitPosition);
+	float l = GetLight(rayHitPosition,rayMarchSettings);
 	vec3 col = vec3(l);
 	
 	fragColor = vec4(col,1.0);
